f270306cbcd19561dace71eab1b7e20af0f8b0c6
parsa-faraji <parsafaraji8213@gmail.com>
2024-09-16 11:06:11 -0700

    factorial program

---
+++ ClassNotes/Sep_16/Sep_16/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  Sep_16
+//
+//  Created by Parsa Faraji on 9/16/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    int result = 1;
+    
+    cout << "Enter a positive number: " << endl;
+    cin >> num;
+    
+    
+    for (int i = 1; i <= num; i++) {
+        result *= i;
+    }
+    
+    cout << "The facotrial of " << num << " is " << result << endl;
+    return 0;
+}
---
+++ ClassNotes/Sep_16/Sep_16/main2.cpp
@@ -0,0 +1,8 @@
+//
+//  main2.cpp
+//  Sep_16
+//
+//  Created by Parsa Faraji on 9/16/24.
+//
+
+#include "main2.hpp"
