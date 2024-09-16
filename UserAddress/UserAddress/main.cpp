b6d5ec793f62c8606bfd3ad7b4b224d90c09bf24
parsa-faraji <parsafaraji8213@gmail.com>
2024-09-15 18:41:11 -0700

    define variables and collect data

--- UserAddress/UserAddress/main.cpp
+++ UserAddress/UserAddress/main.cpp
@@ -6,8 +6,30 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main() {
-
+    // create variables
+    string street;
+    string city;
+    string state;
+    string zipCode;
+    
+    // prompt user and collect data
+    cout << "Please enter your Street Address:\n";
+    getline(cin, street);
+    
+    cout << "Please enter your city:\n";
+    getline(cin, city);
+    
+    cout << "Please enter your state:\n";
+    getline(cin, state);
+    
+    cout << "Please enter your zipcode: ";
+    getline(cin, zipCode);
+    
+    
+    
+    
     return 0;
 }
