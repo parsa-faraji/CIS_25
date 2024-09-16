83395a89e9fbdbe854a12953d03faa3443185ede
parsa-faraji <parsafaraji8213@gmail.com>
2024-09-15 21:28:34 -0700

    implement arrayOfNames

--- NamesArray/NamesArray/main.cpp
+++ NamesArray/NamesArray/main.cpp
@@ -6,9 +6,29 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    string arrayOfNames[3];
+    string name1;
+    string name2;
+    string name3;
+    
+    cout << "Please enter name 1: " << endl;
+    cin >> name1;
+    
+    cout << "Please enter name 2: " << endl;
+    cin >> name2;
+    
+    cout << "Please enter name 3: " << endl;
+    cin >> name3;
+    
+    arrayOfNames[0] = name1;
+    arrayOfNames[1] = name2;
+    arrayOfNames[2] = name3;
+    
+    cout << "Name 1: " << name1 << endl;
+    
+
     return 0;
 }
