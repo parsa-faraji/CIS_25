869ef4d28031d2a0e37b90c5b732a6b8c63e4958
parsa-faraji <parsafaraji8213@gmail.com>
2024-10-08 22:55:48 -0700

    swap two variables

--- ClassNotes/PointerSwap/PointerSwap/main.cpp
+++ ClassNotes/PointerSwap/PointerSwap/main.cpp
@@ -6,9 +6,31 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+int main() {
+    int var1 = 15;
+    int* ptr1 = &var1;
+    
+    int var2 = 30;
+    int* ptr2 = &var2;
+    
+    int temp;
+    int* ptr_temp = &temp;
+    
+    cout << "before: " << endl;
+    cout << "var1: " << var1 << endl;
+    cout << "var2: " << var2 << endl;
+    
+    *ptr_temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = *ptr_temp;
+
+    
+    
+    cout << "after: " << endl;
+    cout << "var1: " << var1 << endl;
+    cout << "var2: " << var2 << endl;
+    
 }
