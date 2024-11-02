47120224ba3354a2b3f508d4c74ec4973ff8eedd
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-01 19:24:34 -0700

    add the methods

--- ClassNotes/classActivity26/classActivity26/Point.h
+++ ClassNotes/classActivity26/classActivity26/Point.h
@@ -8,9 +8,21 @@
 #ifndef Point_h
 #define Point_h
 #include <iostream>
+using namespace std;
 
 struct Point {
+    // attributes
+    int x;
+    int y;
     
+    bool operator==(const Point& point) {
+        return (x == point.x) && (y == point.y)
+    }
+    
+    ostream& operator<<(ostream& stream, const Point& point){
+        stream << "(" << point.x << ", " << point.y << ")";
+        return stream;
+    }
 }
 
 #endif /* Point_h */
