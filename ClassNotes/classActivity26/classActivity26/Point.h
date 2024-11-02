ea2e9920f0422eee41296b4834d4869dd0b2f37c
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-01 19:16:29 -0700

    create struct header file

---
+++ ClassNotes/classActivity26/classActivity26/Point.h
@@ -0,0 +1,16 @@
+//
+//  Point.h
+//  classActivity26
+//
+//  Created by Parsa Faraji on 11/1/24.
+//
+
+#ifndef Point_h
+#define Point_h
+#include <iostream>
+
+struct Point {
+    
+}
+
+#endif /* Point_h */
