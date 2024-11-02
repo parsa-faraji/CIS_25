6af2326644fcef878190d66479d417836b46fcb7
parsa-faraji <parsafaraji8213@gmail.com>
2024-11-01 19:30:43 -0700

    create Point struct

---
+++ ClassNotes/classActivity26/classActivity26/Point.h
@@ -0,0 +1,19 @@
+//
+//  Point.h
+//  classActivity26
+//
+//  Created by Parsa Faraji on 11/1/24.
+//
+
+#ifndef Point_h
+#define Point_h
+#include <iostream>
+
+struct Point {
+    int x;
+    int y;
+
+};
+
+
+#endif /* Point_h */
